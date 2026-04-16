518ef9f03ad3edaaa88a78a216f92349fbe5bede
biuntri <biuntri.213@gmail.com>
2026-04-16 09:43:08 +0900

    障害物が動くようにした

--- Assets/お手本/障害物/ObstacleController.cs
+++ Assets/お手本/障害物/ObstacleController.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour
 {
     [SerializeField] float Speed;
+	[SerializeField] bool isSimulating;
 
+	[Header("Setting")]
+	[SerializeField] Rigidbody2D rigidbody;
 
+	Vector2 Velocity => new(Speed, 0);
+
+	private void Update()
+	{
+		if (!isSimulating) return; //ƒvƒŒƒC’†‚Å‚È‚¯‚ê‚Î‰½‚à‚µ‚È‚¢
+
+		//“®ìŠÇ—
+		rigidbody.linearVelocity = Velocity;
+	}
 }
