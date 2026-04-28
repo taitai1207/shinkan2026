b1203f91baca18e95ad9968ce547e79b574f46b3
biuntri <biuntri.213@gmail.com>
2026-04-28 18:45:45 +0900

    ゲームオーバー表示キャンバスの操作スクリプトを作成

---
+++ Assets/お手本/UI/GameOveCanvasController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameOveCanvasController : MonoBehaviour
+{
+	[SerializeField] Canvas Canvas;
+	public bool GameOverShow
+	{
+		set
+		{
+			Canvas.enabled = value;
+		}
+	}
+
+	private void Start()
+	{
+		Canvas.worldCamera = Camera.main;
+	}
+}
