d3ed1a9a3e44e13b51d88289d96af8feaebb9130
biuntri <biuntri.213@gmail.com>
2026-04-29 23:10:05 +0900

    好ましくない現象が起きたときにアサーション

--- Assets/お手本/障害物/ObstacleSetController.cs
+++ Assets/お手本/障害物/ObstacleSetController.cs
@@ -49,7 +49,7 @@ public class ObstacleSetController : MonoBehaviour
 		//出現直後に場所を調整
 		//x座標
 		transform.position = new(CameraEndPointByWorldPosition[1] * 1.1f, 0, 0);
-		if (CameraEndPointByWorldPosition[0] > 0) Debug.Log("カメラ左端が0以上にあります"); // 不要だと思うけど一応
+		if (CameraEndPointByWorldPosition[0] > 0) Debug.LogAssertion("カメラ左端が0以上にあります"); // 不要だと思うけど一応
 
 		//各障害物のy座標調整
 		float upperSize = UpperObstacle.transform.localScale.y, downerSize = DownerObstacle.transform.localScale.y;
