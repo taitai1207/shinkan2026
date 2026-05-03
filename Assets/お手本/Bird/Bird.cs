ed9ce6d0cf8b2c5becd0b1209db355a6c242c0d0
Paul <paul.kr.anderson@gmail.com>
2026-05-04 03:20:00 +0900

    再起動時に速度をリセット

--- Assets/お手本/Bird/Bird.cs
+++ Assets/お手本/Bird/Bird.cs
@@ -5,7 +5,7 @@ public class Bird : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
     private GameManager gameManager;
-    private float force = 2.5f;
+    private float force = 5.0f;
     private float cooldownExpiry = 0;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,6 +21,8 @@ public class Bird : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && !gameManager.IsGameOver && Time.time > cooldownExpiry)
         {
             Vector2 upForce = new Vector2(0.0f, force); // 力の方向と大きさ
+
+            rigidbody.linearVelocity = new Vector2(0.0f, 0.0f);
             rigidbody.AddForce(upForce, ForceMode2D.Impulse);
             cooldownExpiry = Time.time + 0.2f;
         }
@@ -29,6 +31,7 @@ public class Bird : MonoBehaviour
 	public void GameReset()
 	{
 		this.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+        rigidbody.linearVelocity = new Vector2(0.0f, 0.0f);
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
